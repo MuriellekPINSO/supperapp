08097b53ab998d578b0b70d9d403354678d4ee1c
Muri <muriellekpinso@gmail.com>
2026-02-27 15:39:55 +0100

    Initial Commit

---
+++ supperapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  supperapp
+//
+//  Created by Murielle KPINSO  on 27/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ supperapp/supperappApp.swift
@@ -0,0 +1,17 @@
+//
+//  supperappApp.swift
+//  supperapp
+//
+//  Created by Murielle KPINSO  on 27/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct supperappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
